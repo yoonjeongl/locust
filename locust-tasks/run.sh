0a750d31067c36998412ff40207d64ef2c76d7b6
yoonjeongl <58279361+yoonjeongl@users.noreply.github.com>
2021-07-20 11:24:21 +0900

    Update run.sh

--- locust-tasks/run.sh
+++ locust-tasks/run.sh
@@ -19,9 +19,9 @@ LOCUS_OPTS="-f /locust-tasks/locustfile.py"
 LOCUST_MODE=${LOCUST_MODE:-standalone}
 
 if [[ "$LOCUST_MODE" = "master" ]]; then
-    LOCUS_OPTS="$LOCUS_OPTS —master"
+    LOCUS_OPTS="$LOCUS_OPTS —-master"
 elif [[ "$LOCUST_MODE" = "worker" ]]; then
-    LOCUS_OPTS="$LOCUS_OPTS —worker —master-host=$LOCUST_MASTER"
+    LOCUS_OPTS="$LOCUS_OPTS —-worker —-master-host=$LOCUST_MASTER"
 fi
 
 echo "$LOCUST $LOCUS_OPTS"
